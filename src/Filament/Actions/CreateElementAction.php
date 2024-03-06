8f12ff1eed839b511f79e67d9f03356f6b7cf0db
tiagof <tiagof@users.noreply.github.com>
2024-03-06 18:32:16 +0000

    Fix styling

--- src/Filament/Actions/CreateElementAction.php
+++ src/Filament/Actions/CreateElementAction.php
@@ -6,7 +6,7 @@ use Filament\Tables\Actions\CreateAction;
 
 class CreateElementAction extends CreateAction
 {
-    public static function make(string $name = null): static
+    public static function make(?string $name = null): static
     {
         return parent::make($name)
             ->mutateFormDataUsing(ElementPayloadActions::mutateFormDataCallable());
