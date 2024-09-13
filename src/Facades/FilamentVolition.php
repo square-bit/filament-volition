59913fcf7efaed694cd6c165b97dff49797dd5b0
Tiago Fernandes <tiagoff@gmail.com>
2024-09-13 11:34:47 +0100

    Update FilamentVolition.php

--- src/Facades/FilamentVolition.php
+++ src/Facades/FilamentVolition.php
@@ -9,11 +9,7 @@ use Illuminate\Support\Facades\Facade;
  * @see \Squarebit\FilamentVolition\FilamentVolition
  *
  * @method static self registerVolitionals(array|string $volitionals)
- * @method static self registerConditions(array|string $condition)
- * @method static self registerActions(array|string $action)
  * @method static array volitionals()
- * @method static array conditions()
- * @method static array actions()
  * @method static array<int, Block> getFilamentBlocksForConditions()
  * @method static array<int, Block> getFilamentBlocksForActions()
  */
