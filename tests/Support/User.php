06d6f6b18ac46b0b76171e76472cddab2554c9fc
Tiago Fernandes <tiagoff@gmail.com>
2025-01-19 12:56:03 +0000

    Remove unnecessary dependency

--- tests/Support/User.php
+++ tests/Support/User.php
@@ -9,13 +9,11 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract, FilamentUser
 {
     use Authenticatable;
     use Authorizable;
-    use HasRoles;
 
     protected $fillable = ['email'];
 
